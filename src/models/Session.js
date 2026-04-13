5fff600d78f05783d8554cf09fad6a45b4caf85c
Iris Le <irisle5863@gmail.com>
2026-04-13 14:20:23 -0500

    added Session and Movie models

---
+++ src/models/Session.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+//blueprint for a movie session in the session pool
+const movieSchema = new mongoose.Schema({
+    tmdbId: { type: String, required: true },
+    title: { type: String, required: true },
+    poster: { type: String },
+    year: { type: String },
+    genre: { type: [String] },
+    rating: { type: Number },
+    runtime: { type: Number },
+    overview: { type: String },
+    addedBy: { type: String, required: true }
+});
+
+//blueprint for a session
+const sessionSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true,
+        length: 6
+    },
+    host: {
+        type: String,
+        required: true
+    },
+    participants: {
+        type: [String],
+        default: []
+    },
+    movies: {
+        type: [movieSchema],
+        default: []
+    },
+    status: {
+        type: String,
+        enum: ['stepup', 'voting', 'completed'],
+        default: 'setup'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expries: '24h' // Session expires after 24 hours
+    }
+})
+
+module.exports = mongoose.model('Session', sessionSchema);
